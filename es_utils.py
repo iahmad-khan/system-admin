1325853ef5889c8594f6f7893b08924ff4a8754a
Ijaz ahmad khan <ijazahmad722@gmail.com>
2016-02-22 08:03:05 +0100

    Update es_utils.py

--- es_utils.py
+++ es_utils.py
@@ -7,13 +7,11 @@ def send_payload(index,document,id,payload):
     passw=open('/data/secrets/github_hook_secret_cmsbot','r').read().strip()
   except Exception as e:
     print "Couldn't read the secrets file" , str(e)
-
-  #url="http://%s/%s/%s/%s" % ('cmses-client01.cern.ch',index,document,id)
+  #form the url with index name , doc name , and uniq id
   url="http://%s/%s/%s/%s" % ('cmses-master01.cern.ch:9200',index,document,id)
-  #print url
   #send the data to elasticsearch
   passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-  passman.add_password(None,url, 'elasticsearch', passw)
+  passman.add_password(None,url, 'user_name', passw)
   auth_handler = urllib2.HTTPBasicAuthHandler(passman)
   opener = urllib2.build_opener(auth_handler)
   try:
