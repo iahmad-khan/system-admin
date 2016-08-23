9ae91293635607926d17face06166adb64a709d4
Ijaz ahmad khan <ijazahmad722@gmail.com>
2016-08-23 11:55:56 +0200

    Update fix-screen-cc7.bash

--- fix-screen-cc7.bash
+++ fix-screen-cc7.bash
@@ -5,3 +5,23 @@ gtf 1600 900 60 -x
 xrandr --newmode "1600x900" 119.00  1600 1696 1864 2128  900 901 904 932  -HSync +Vsync
 xrandr --addmode VGA1 1600x900
 xrandr --output VGA1 --mode 1600x900
+
+
+or place the script in /etc/systemd/system:
+  
+  
+[Unit]
+Description=fixdisplay
+After=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/root/fixdisplay.sh
+Type=forking
+PIDFile=/var/run/fixdisplay.pid
+User=root
+Group=root
+
+[Install]
+WantedBy=default.target
+
